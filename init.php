98fdd77d85952b353f6709dee1dd3bd2ef0bc3f2
Thomas Pondelek <thomas.pondelek@page-effect.com>
2024-07-11 15:08:53 +0200

    starting init.php

---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: No bloat Popups 
+ * Plugin URI: https://page-effect.de/
+ * Description: Test.
+ * Version: 0.1
+ * Author: Page Effect
+ * Author URI: https://page-effect.de/
+ **/
