0f0bbbb5108b6def6b2800aff5c3f5fe324d5882
Thomas Pondelek <thomas.pondelek@page-effect.com>
2026-02-19 16:16:50 +0100

    messe

--- init.php
+++ init.php
@@ -21,17 +21,38 @@ add_action(
 /// Add Popup to Footer
 
 
-if (!isset($_COOKIE["visit_time"])) {
+// if (!isset($_COOKIE["visit_time"])) {
+  //testing
+  if(1==1) {
 
   add_action('wp_footer', 'pe_popup');
 }
 
 
+// Störer
+// function pe_popup()
+// {
+//   $pop = "<div class='pe-pop'> <div class='date'>Ab dem 20. Januar 2025</div> sind wir auch im <div class='place'>Gesundheitszentrum</div> <div class='place'>Karlstadt</div> für Sie da.</div>";
+//   echo $pop;
+// }
 
-function pe_popup()
-{
-  $pop = "<div class='pe-pop'> <div class='date'>Ab dem 20. Januar 2025</div> sind wir auch im <div class='place'>Gesundheitszentrum</div> <div class='place'>Karlstadt</div> für Sie da.</div>";
-  echo $pop;
+
+// Gewinnspiel
+function pe_popup() {
+  $pop = "<div class='pe-pop'> 
+<div class='container'>
+<button class='close' aria-label='Fenster schließen'>X</button>
+<h2 class='headline-2'>JETZT HÖRTEST MACHEN</br>
+UND E-BIKE GEWINNEN!</h2>
+
+<p>Zum Tag der Hörgesundheit in Lohr
+am Samstag, den 11.04.26 (10-15 Uhr) in der
+Alten Turnhalle Lohr a.Main verlost Hörakustik Döll
+unter allen Teilnehmer:innen ein E-Bike</p>
+ </div>   </div>";
+ 
+
+echo $pop;
 }
 
 
@@ -43,9 +64,9 @@ function pe_popup()
 
 function pe_enqueue()
 {
-  wp_register_style('pop_style', plugin_dir_url(__FILE__) . 'pop-style.css', true, '1.0.0');
+  wp_register_style('pop_style', plugin_dir_url(__FILE__) . 'pop-style.css', true, '1.0.1');
   wp_enqueue_style('pop_style');
-  wp_register_script('pop', plugin_dir_url(__FILE__) . 'pop.js', array('jquery'), true, '1.0.0');
+  wp_register_script('pop', plugin_dir_url(__FILE__) . 'pop.js', array('jquery'), true, '1.0.1');
   wp_enqueue_script('pop');
 }
 
--- pop.js
+++ pop.js
@@ -2,7 +2,14 @@ var $ = jQuery.noConflict();
 
 $(document).ready(function () {
   $(".pe-pop").show();
-  setTimeout(function () {
-    $(".pe-pop").fadeOut(1500);
-  }, 10000);
+
+  //Close Popup auto after 10 seconds
+  // setTimeout(function () {
+  //   $(".pe-pop").fadeOut(1500);
+  // }, 10000);
+
+  //Close Popup on close button click
+  $(".pe-pop .close").click(function () {
+    $(this).closest(".pe-pop").fadeOut(1500);
+  });
 });
